d0f0965404b1389f377e854b72badbf7b22d9048
tberti81 <tberti@freemail.hu>
2017-06-01 23:13:41 +0200

    added animal list as an input

--- src/AppBundle/Command/AnimalSortCommand.php
+++ src/AppBundle/Command/AnimalSortCommand.php
@@ -3,42 +3,64 @@
 namespace AppBundle\Command;
 
 use AppBundle\Command\Animal\Animal;
-use AppBundle\Command\Animal\Bird;
-use AppBundle\Command\Animal\Dog;
-use AppBundle\Command\Animal\Snake;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use InvalidArgumentException;
 
+/**
+ * @package AppBundle\Command
+ */
 class AnimalSortCommand extends Command
 {
 	protected function configure()
 	{
 		$this
 			->setName('Animal sorter')
-			->setAliases(['as']);
+			->setAliases(['as'])
+			->setDescription('Sort the listed animals by leg count(desc) and name(asc).')
+			->addArgument(
+				'animals',
+				InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+				'List the animals in the following way... type:name type:name type:name (separate each type and name pair with a comma):'
+			)
+		;
 	}
 
+	/**
+	 * @param InputInterface  $input
+	 * @param OutputInterface $output
+	 *
+	 * @return void
+	 *
+	 * @throws InvalidArgumentException
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$dogAlbert = new Dog('Albert');
-		$snakeSly  = new Snake('Sly');
-		$dogWilly  = new Dog('Willy');
-		$birdRusty = new Bird('Rusty');
-		$dogBuddy  = new Dog('Buddy');
+		$inputAnimals = $input->getArgument('animals');
 
-		$animals = [
-			$this->getAnimalId($dogAlbert) => $dogAlbert,
-			$this->getAnimalId($snakeSly)  => $snakeSly,
-			$this->getAnimalId($dogWilly)  => $dogWilly,
-			$this->getAnimalId($birdRusty) => $birdRusty,
-			$this->getAnimalId($dogBuddy)  => $dogBuddy,
-		];
-		var_dump($animals);
+		/** @var Animal[] $animals */
+		$animals = [];
+
+		foreach ($inputAnimals as $inputAnimal)
+		{
+			list($type, $name) = explode(':', $inputAnimal);
+
+			$animalClass = 'AppBundle\Command\Animal\\' . ucfirst($type);
+
+			if (!class_exists($animalClass))
+			{
+				throw new InvalidArgumentException(sprintf('The \'%s\' animal is not implemented!', $type));
+			}
+
+			$animal = new $animalClass(ucfirst($name));
+
+			$animals[$this->getAnimalId($animal)] = $animal;
+		}
 
 		foreach ($animals as $key => $animal)
 		{
-			/** @var Animal $animal */
 			$animalsToSort[$key] = [
 				'name'         => $animal->getName(),
 				'numberOfLegs' => $animal->getNumberOfLegs()
@@ -50,15 +72,23 @@ class AnimalSortCommand extends Command
 
 		array_multisort($numberOfLegs, SORT_DESC, $names, SORT_ASC, $animalsToSort);
 
+		$output->writeln('Here are the sorted animals:');
+
 		/** @var Animal[] $result */
 		$result = [];
 		foreach ($animalsToSort as $key => $animalToSort)
 		{
 			$result[] = $animals[$key];
+
+			$output->writeln(str_replace('AppBundle\Command\Animal\\', '', get_class($animals[$key])) . ' ' . $animals[$key]->getName());
 		}
-		var_dump($result);
 	}
 
+	/**
+	 * @param Animal $animal
+	 *
+	 * @return string
+	 */
 	private function getAnimalId(Animal $animal)
 	{
 		return get_class($animal) . '_' . $animal->getNumberOfLegs() . '_' . $animal->getName();
